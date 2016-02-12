46686813d928d90e7d97a96f7d7aed2372543aea
arthur-balagne <arthur97115@gmail.com>
2016-02-12 15:27:32 +0100

    added js in gulpfile

---
+++ app/js/test.js
@@ -0,0 +1,19 @@
+console.log('Ok');
+alert('ok');
+
+//babel
+
+function fibonacci(n) {
+  let previous = 0;
+  let current = 1;
+
+  for(let i = 0; i < n; i += 1) {
+    let temp = previous;
+    previous = current;
+    current = temp + current;
+  }
+
+  return current;
+}
+
+console.log(fibonacci(8));
--- functions.php
+++ functions.php
@@ -114,15 +114,27 @@ add_action( 'widgets_init', 'hackathon_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function hackathon_theme_scripts() {
+
 	wp_enqueue_style( 'hackathon_theme-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'hackathon_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'hackathon_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	//libs
+
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri(). '/lib/css/bootstrap.css' );
+
+    wp_enqueue_script( 'hackathon_theme-jQuery', get_template_directory_uri() . '/lib/js/jquery.js' );
+
+    wp_enqueue_script( 'bootstrap-script', get_template_directory_uri(). '/lib/js/bootstrap.js' );
+
+    // custom scripts
+
+	wp_enqueue_style( 'hackathon_theme_custom-style', get_template_directory_uri(). '/dist/css/app.css' );
+
+    wp_enqueue_script( 'hackathon_theme-script', get_template_directory_uri(). '/dist/js/app.js' );
+
 }
 add_action( 'wp_enqueue_scripts', 'hackathon_theme_scripts' );
 
